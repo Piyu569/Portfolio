66264601435bbb580bd2a471147f355aef715532
piyushbodhani1 <piyush.bodhani@kpipartners.com>
2023-08-16 11:54:33 +0530

    FC

---
+++ Myapp/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from Myapp import views
+
+urlpatterns = [
+    path('index', views.index, name= 'index'),
+    path('projects', views.projects, name= 'projects'),
+    path('experience', views.experience, name= 'experience'),
+    path('contact', views.contact, name= 'contact'),
+    path('contact_success', views.contact_me, name= 'contact'),
+
+
+]
