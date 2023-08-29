f27e74a067374829a51288fb5ad4f8e0351c62e5
Piyu569 <piyushbodhani01@gmail.com>
2023-08-29 23:46:13 +0530

    u

--- Myapp/urls.py
+++ Myapp/urls.py
@@ -6,10 +6,7 @@ urlpatterns = [
     path('index', views.index, name= 'index'),
     path('projects', views.projects, name= 'projects'),
     path('experience', views.experience, name= 'experience'),
-    path('contact', views.contact, name= 'contact'),
-    path('contact_success', views.contact_me, name= 'contact'),
-
-
+    path('aboutme', views.aboutme, name= 'aboutme'),
 ]
 # from django.contrib import admin
 # from django.urls import path
